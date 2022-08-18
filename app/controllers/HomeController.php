f7034c482a45d2b0109682213195d8939cc0fc31
javadkhaleghi <46539404+javadkhaleghi@users.noreply.github.com>
2022-08-18 18:30:04 +0430

    Site home page edited.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,12 +2,32 @@
 
 namespace App\Controllers;
 
-use Core\{Controller, Database, Session};
+use App\Models\{Article, Category};
+use Core\{Controller, Router, Session};
 
 class HomeController extends Controller
 {
     public function indexAction()
     {
+        $categoryParams = [
+            'order' => 'name',
+            'limit' => 6
+        ];
+        
+        $articleParams = [
+            'columns' => 'articles.*, users.first_name, users.last_name, categories.name AS category, categories.id AS category_id',
+            'conditions' => "articles.status = :status",
+            'bind' => ['status' => 'public'],
+            'joins' => [
+                ['users', 'articles.user_id = users.id'],
+                ['categories', 'articles.category_id = categories.id', 'categories', 'LEFT']
+            ],
+            'order' => 'articles.created_at DESC',
+            'limit' => 5
+        ];
+        
+        $this->view->categories = Category::find($categoryParams);
+        $this->view->articles = Article::find($articleParams);
         $this->view->setSiteTitle('Home :: PHP MVC');
         $this->view->render();
     }
